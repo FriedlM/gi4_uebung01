b2836837ccb12957c98f58cf3541f6d838ce6c76
Marian Friedl <marian.friedl@rwth-aachen.de>
2015-04-15 22:17:46 +0200

    improved isset, added functionality for parameter -v

--- isset.c
+++ isset.c
@@ -4,14 +4,24 @@ int main(int argc, char* argv[], char* envp[])
 {
 	if (argc > 1)
 	{
-		char* value = getenv(argv[1]);
+		int v = 0;
+		char* value;
+
+		if (argc > 2)
+		{
+			if (strcmp(argv[1], "-v") == 0)
+				v++;
+		}
+		
+		value = getenv(argv[1+v]);
 		if (value != NULL)
 		{
-			printf("%s = %s\n", argv[1], value);
+			if (v)
+				printf("%s = %s\n", argv[1+v], value);
 			return 0; 
 		}
-		else
-			printf("%s ist nicht gesetzt.\n", argv[1]);
+		else if (v)
+			printf("%s ist nicht gesetzt.\n", argv[1+v]);
   	}
 	else
 		printf("Bitte eine Umgebungsvariable als Parameter angeben.\n");
